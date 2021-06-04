ce42aa863a4e571fcabc5ad81d967af82336e8ad
Jordan <jtang0819@gmail.com>
2021-06-04 09:48:26 -0400

    06042021 update

--- csv_parser.py
+++ csv_parser.py
@@ -1,45 +1,45 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,IntegerType,TimestampType,DecimalType,StringType,DateType,StructField
+from pyspark.sql.types import StructType, IntegerType, TimestampType, DecimalType, StringType, DateType, StructField
 from typing import List
 import config
 
 
 common_event = StructType([
-    StructField("col1_val", DateType()),
-    StructField("col2_val", StringType()),
-    StructField("col3_val", StringType()),
-    StructField("col4_val", StringType()),
-    StructField("col5_val", TimestampType()),
-    StructField("col6_val", IntegerType()),
-    StructField("col7_val", TimestampType()),
-    StructField("col8_val", DecimalType()),
-    StructField("col9_val", DecimalType()),
-    StructField("col10_val", IntegerType()),
-    StructField("col11_val", DecimalType()),
-    StructField("col12_val", IntegerType()),
-    StructField("col13_val", StringType())
+    StructField("trade_dt", DateType()),
+    StructField("arrival_tm", TimestampType()),
+    StructField("rec_type", StringType()),
+    StructField("symbol", StringType()),
+    StructField("event_tm", TimestampType()),
+    StructField("event_seq_nb", IntegerType()),
+    StructField("exchange", StringType()),
+    StructField("trade_pr", DecimalType()),
+    StructField("bid_pr", DecimalType()),
+    StructField("bid_size", IntegerType()),
+    StructField("ask_pr", DecimalType()),
+    StructField("ask_size", IntegerType()),
+    StructField("partition", StringType())
 ])
 
+# 2020-08-06,2020-08-06 09:30:00.0,Q,SYMA,2020-08-06 18:08:58.256,69,NYSE,75.54449587635618,100,77.16118144376208,100
+# 2020-08-06,2020-08-06 09:30:00.0,T,SYMA,2020-08-06 18:14:28.899,70,NYSE,78.23471404145394,125
 
-def parse_csv(line:str):
+
+def parse_csv(line: str):
     record_type_pos = 2
     record = line.split(",")
     try:
     # [logic to parse records]
         if record[record_type_pos] == "T":
             event = [record[0], record[1], record[2], record[3],
-                     record[4], record[5], record[6], record[7], record[8],
-                     record[9], record[10]]
+                     record[4], record[5], record[6], record[7], "", "", "", "", "T"]
             return event
         elif record[record_type_pos] == "Q":
             event = [record[0], record[1], record[2], record[3], record[4],
-                     record[5], record[6], record[7], record[8],
-                     record[9], record[10]]
+                     record[5], record[6], "", record[7], record[8],
+                     record[9], record[10], "Q"]
             return event
-    except record[record_type_pos] == "":
-        event = [record[0], record[1], record[2], record[3],
-                 record[4], record[5], record[6], record[7], record[8],
-                 "B", record[10]]
+    finally:
+        event = ["", "", "", "", "", "", "", "", "", "", "", "", "B"]
     return event
 
 
@@ -47,16 +47,21 @@ def apply_latest(line):
     return line.groupBy()
 
 
-spark = SparkSession.builder.master('local').appName('app').getOrCreate()
-spark.conf.set("fs.azure.account.key.springboardstoragejt.blob.core.windows.net", config.access_token)
-raw = spark.textFile("wasbs://blob-container@springboardstoragejt.blob.core.windows.net/data/csv/2020-08-05/NYSE/part-00000-5e4ced0a-66e2-442a-b020-347d0df4df8f-c000.txt")
+appName = 'Springboard data ingestion project'
+spark = SparkSession.builder.master('local').appName(appName).getOrCreate()
+raw = spark.sparkContext.textFile("file:///C:/Users/jordan/Python-Projects/springboard-capstone2/Springboard-data-ingestion-project/2020-08-06_trade_data.txt")
+# print(raw.top(10))
 parsed = raw.map(lambda line: parse_csv(line))
-data = spark.createDataFrame(parsed)
-data.write.partitionBy("partition").mode("overwrite").parquet("output_dir")
+# print(parsed.top(10))
+data = spark.createDataFrame(parsed, common_event)
+# print(data.show(10))
+data.write.partitionBy("partition").mode("overwrite").parquet("output_dir/data.parquet")
 
-trade_common = spark.read.parquet("output_dir/partition=T")
-trade = trade_common.select('trade_dt', 'symbol', 'exchange', 'event_tm', 'event_seq_nb', 'file_tm', 'trade_pr')
-trade_corrected = apply_latest(trade)
-trade_date = '2020-07-29'
-trade.write.parquet("data/csv/2020-08-05/NYSE/trade/trade_dt={}".format(trade_date))
-
+# trade_common = spark.read.parquet("output_dir/partition=T")
+# trade = trade_common.select('trade_dt', 'symbol', 'exchange', 'event_tm', 'event_seq_nb', 'file_tm', 'trade_pr')
+# trade_corrected = apply_latest(trade)
+# trade_date = '2020-07-29'
+# trade.write.parquet("data/csv/2020-08-05/NYSE/trade/trade_dt={}".format(trade_date))
+#
+#
+# df = spark.read.parquet('data/')
