998ac40c6ab469281f1f14a1201a2353478fb4f2
Jordan <jtang0819@gmail.com>
2021-06-11 10:47:30 -0400

    06012021 stepfour update

--- analytical.py
+++ analytical.py
@@ -23,13 +23,47 @@ last_pr_df = spark.sql(
     """select symbol, exchange, last_pr from (select
 symbol, exchange, event_tm, event_seq_nb, trade_pr,
 avg(trade_pr) over(partition by symbol order by event_tm range between interval '30' MINUTES PRECEDING AND CURRENT ROW) 
+as last_pr
 FROM tmp_trade_moving_avg) a
 """)
 last_pr_df.write.saveAsTable("temp_last_trade")
 quote_union = spark.sql(
     """
-    create view quote_union AS
-    
+    SELECT a.trade_dt, b.rec_type, a.symbol, a.event_tm, b.event_seq_nb,
+    a.exchange, b.bid_pr, b.bid_size, b.ask_size, b.trade_pr, b.mov_avg_pr
+    from
+    quotes a
+    join temp_trade_moving_avg b on a.arrival_tm = b.event_tm 
     """
 )
-
+quote_union.createOrReplaceTempView("quote_union")
+quote_union_update = spark.sql(
+    """
+    select *,
+    max(trade_pr) as last_trade_pr,
+    max(mov_avg_pr) as last_move_avg_pr
+    from quote_union
+    """
+)
+quote_union_update.createOrReplaceTempView("quote_union_update")
+quote_update = spark.sql(
+    """
+    select trade_dt, symbol, event_tm, event_seq_nb, exchange,
+    bid_pr, bid_size, ask_pr, ask_size, last_trade_pr, last_mov_avg_pr
+    from quote_union_update
+    where rec_type = 'Q'
+    """
+)
+quote_update.createOrReplaceTempView("quote_update")
+quote_final = spark.sql(
+    """
+    select a.trade_dt, a.symbol, a.event_tm, a.event_seq_nb, a.exchange,
+    a.bid_pr, a.bid_size, a.ask_pr, a.ask_size, a.last_trade_pr, a.last_mov_avg_pr,
+    a.bid_pr - b.close_pr as bid_pr_mv, a.ask_pr - b.close_pr as ask_pr_mv
+    from 
+    quote_update a left outer join tmp_last_trade b on 
+    # [Broadcast temp_last_trade table. Use quote_update to left outer join
+    temp_last_trade]
+    """
+)
+quote_final.write.parquet('/quote-trade-analytical/date={}'.format(trade_date))
--- csv_parser.py
+++ csv_parser.py
@@ -63,20 +63,78 @@ raw = spark \
     .sparkContext \
     .textFile(
     "file:///C:/Users/jordan/Python-Projects/springboard-capstone2/Springboard-data-ingestion-project/2020-08-06_trade_data.txt")
-# print(raw.top(10))
 parsed = raw.map(lambda line: parse_csv(line))
-# print(parsed.dtypes)
-# print(parsed.head(10))
 data = spark.createDataFrame(parsed, common_event)
 data.write.partitionBy("partition").mode("overwrite").parquet("output_dir/data.parquet")
 # STEP THREE
 trade_common = spark.read.parquet("output_dir/data.parquet/partition=T/*")
-# print(trade_common.schema)
 trade = trade_common.select('trade_dt', 'symbol', 'exchange', 'event_tm', 'event_seq_nb', 'arrival_tm', 'trade_pr')
 trade_corrected = apply_latest(trade)
 trade_date = '2020-07-29'
 trade.write.parquet("output_dir/trade/trade_dt={}".format(trade_date))
+df = spark.read.parquet(('output_dir/trade/trade_dt={}'.format('2020-07-29')))
+df = spark.sql("select symbol, event_tm, event_seq_nb, trade_pr from trades where trade_dt = '2020-07-20'")
+mov_avg_df = spark.sql(
+    """
+    select symbol, exchange, event_tm, event_seq_nb, trade_pr, 
+    avg(trade_pr) over(partition by symbol order by event_tm) as mov_avg_pr
+    from tmp_trade_moving_avg
+    """
+)
+mov_avg_df.write.saveAsTable("temp_trade_moving_avg")
 
+date = datetime.datetime.strptime('2020-07-29', '%Y-%m-%d')
+prev_date_str = datetime.datetime.today() - datetime.timedelta(days=1)
+df = spark.sql("select symbol, event_tm, event_seq_nb, trade_pr from trades where trade_dt = '{}'".format(prev_date_str))
+df.createOrReplaceTempView("tmp_last_trade")
+last_pr_df = spark.sql(
+    """select symbol, exchange, last_pr from (select
+symbol, exchange, event_tm, event_seq_nb, trade_pr,
+avg(trade_pr) over(partition by symbol order by event_tm range between interval '30' MINUTES PRECEDING AND CURRENT ROW) 
+as last_pr
+FROM tmp_trade_moving_avg) a
+""")
+last_pr_df.write.saveAsTable("temp_last_trade")
+quote_union = spark.sql(
+    """
+    SELECT a.trade_dt, b.rec_type, a.symbol, a.event_tm, b.event_seq_nb,
+    a.exchange, b.bid_pr, b.bid_size, b.ask_size, b.trade_pr, b.mov_avg_pr
+    from
+    quotes a
+    join temp_trade_moving_avg b on a.arrival_tm = b.event_tm 
+    """
+)
+quote_union.createOrReplaceTempView("quote_union")
+quote_union_update = spark.sql(
+    """
+    select *,
+    max(trade_pr) as last_trade_pr,
+    max(mov_avg_pr) as last_move_avg_pr
+    from quote_union
+    """
+)
+quote_union_update.createOrReplaceTempView("quote_union_update")
+quote_update = spark.sql(
+    """
+    select trade_dt, symbol, event_tm, event_seq_nb, exchange,
+    bid_pr, bid_size, ask_pr, ask_size, last_trade_pr, last_mov_avg_pr
+    from quote_union_update
+    where rec_type = 'Q'
+    """
+)
+quote_update.createOrReplaceTempView("quote_update")
+quote_final = spark.sql(
+    """
+    select a.trade_dt, a.symbol, a.event_tm, a.event_seq_nb, a.exchange,
+    a.bid_pr, a.bid_size, a.ask_pr, a.ask_size, a.last_trade_pr, a.last_mov_avg_pr,
+    a.bid_pr - b.close_pr as bid_pr_mv, a.ask_pr - b.close_pr as ask_pr_mv
+    from 
+    quote_update a left outer join tmp_last_trade b on 
+    # [Broadcast temp_last_trade table. Use quote_update to left outer join
+    temp_last_trade]
+    """
+)
+quote_final.write.parquet('/quote-trade-analytical/date={}'.format(trade_date))
 
 #
 #
